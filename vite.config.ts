1ae939f505a3d200cae934d58a959e607ff57ca3
Gujja Sai Ganesh <141315542+gujjasaiganesh@users.noreply.github.com>
2025-05-25 22:18:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio-main/',
+  base: './', // 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -19,3 +19,4 @@ export default defineConfig({
     },
   },
 });
+
